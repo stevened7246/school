3d6ee224fe53b8e150bd4c333058d642fe6c74e3
stevened7246 <stevened7246@gmail.com>
2022-09-15 22:09:28 +0800

    0.2.0

--- 菜單/main.py
+++ 菜單/main.py
@@ -5,12 +5,10 @@ import time
 import os
 import json
 from onepush import notify
-import sys
 
 
-webhook1=os.getenv('webhook')
 
-def hash_sha(url):
+def hash_sha(url):#哈希與分辨函數
     a=requests.get(url)
     a.encoding='utf-8'
     a=hashlib.sha3_256(a.text.encode('utf-8')).hexdigest()
@@ -24,23 +22,20 @@ def hash_sha(url):
     log(b+':'+a)
     return a
 
-def log(str='',logfile='./log.txt'):
-    if str!='':print(str)
+def log(str1='',logfile='./log.txt'):
+    if str1!='':print(str1)
     
     logfile=open(logfile,'a',encoding='utf8')
-    logfile.write(str+'\n')
+    logfile.write(str1+'\n')
     logfile.close()
 
-def main():
-    log()
-    log(time.strftime("%Y{}%m{}%d{} %H{}%M{}%S{}").format("年","月","日","时","分","秒")+'\n')
-
+def getdata():
     a=requests.get('http://www2.csic.khc.edu.tw/store/index.asp')
     a.encoding = 'utf-8'
-    c=BeautifulSoup(a.text,features="lxml").body.p.text.replace(' ','').replace('\n','')
+    c=BeautifulSoup(a.text,features="lxml").body.p.text.replace(' ','').replace('\n','').replace('\r','')
     log(c)
     print()
-
+    log('實時數據')
     data1=hash_sha('http://www2.csic.khc.edu.tw/store/data/1.pdf')
     data2=hash_sha('http://www2.csic.khc.edu.tw/store/data/2.pdf')
     data3=hash_sha('http://www2.csic.khc.edu.tw/store/data/3.pdf')
@@ -50,17 +45,31 @@ def main():
     data['data1']=data1
     data['data2']=data2
     data['data3']=data3
+    return data
 
+def main():
+    try:
+        webhook1=os.getenv('webhook')
+    except:
+        log('環境變量有問題，請檢查')
+        exit()
+    log()
+    log(time.strftime("%Y{}%m{}%d{} %H{}%M{}%S{}").format("年","月","日","时","分","秒")+'\n')
+
+    data=getdata()
 
     jsonfilestatus=False
     if os.path.isfile('data.json'):
         jsonfilesize=os.path.getsize('./data.json')
-        print(jsonfilesize)
-        if jsonfilesize>0:
+        print("json大小："+str(jsonfilesize))
+
+        if jsonfilesize>0:#json內部有數據
             jsonfilestatus=True
             jsonfile=open('data.json','r',encoding='utf8')
             jsondata=json.loads(jsonfile.read())
             jsonfile.close()
+
+            print()
             print(jsondata)
  
 
@@ -68,14 +77,16 @@ def main():
     if jsonfilestatus:
         if 'date'and'data1'and'data2'and'data3'in jsondata.keys():
             if data['data']==jsondata['data'] and data['data1']==jsondata['data1'] and data['data2']==jsondata['data2'] and data['data3']==jsondata['data3']:
-                print('ok')
+                print('菜單數據無變化')
             else:
-                print(1111111)
+                print('菜單數據異動')
                 notify('discord',
                 webhook=webhook1,
-                title='OnePush'+'菜單提醒',
-                content='菜單數據異動，請檢查',
-                username="菜單更新小幫手")
+                title='菜單提醒',
+                content='菜單已更新',
+                username="沙雕bot")
+    else:
+        print('第一次運行')
     
 
 
